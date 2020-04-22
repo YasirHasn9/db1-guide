02d837993accabd876ef61e18036d6223fa80d9f
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-21 18:39:01 -0700

    delete a message

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -61,23 +61,31 @@ router.post("/", async (req, res, next) => {
 });
 
 router.put("/:id", async (req, res, next) => {
-    try{
-        const payload = {
-            title: req.body.title,
-            contents: req.body.contents
-        }
-        let id = await db("messages").where("id" , req.params.id).update(payload);
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents
+    };
+    let id = await db("messages")
+      .where("id", req.params.id)
+      .update(payload);
 
-        const updateMessage = await db("messages").where("id" , id).first();
-        res.json(updateMessage)
-    }
-    catch(err){
-        next(err)
-    }
+    const updateMessage = await db("messages")
+      .where("id", id)
+      .first();
+    res.json(updateMessage);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.delete("/:id", async (req, res, next) => {
-
+  try {
+    await db("messages").where("id", req.params.id).del();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
